be9244dce408f60718fa06ab36f7e1fd207cec19
Nicolas Maligue <nico.maligue@gmail.com>
2022-07-12 17:01:36 +0200

    indente nav.js

--- react/src/components/Nav.js
+++ react/src/components/Nav.js
@@ -14,10 +14,12 @@ class Nav extends Component {
   render() {
     return (
         <>
-          <Routes><Route path="/quote" element={<Quote/>}/>
+        <Routes>
+            <Route path="/quote" element={<Quote/>}/>
             <Route path="/order" element={<Order/>}/>
             <Route path="/invoice" element={<Invoice/>}/>
-            <Route path="/customer" element={<Customer/>}/></Routes>
+            <Route path="/customer" element={<Customer/>}/>
+        </Routes>
           <nav className="navbar navbar-expand navbar-dark bg-dark">
             <a className="navbar-brand ps-3" href="index.html">Start
               Bootstrap</a>
@@ -33,33 +35,16 @@ class Nav extends Component {
             <ul className="nav nav-pills flex-column mb-auto">
               <li className="nav-item"><NavLink to="/">Dashboard</NavLink></li>
               <li className="nav-item"><NavLink to="/quote">Devis</NavLink></li>
-              <li className="nav-item"><NavLink to="/order">Commande</NavLink>
-              </li>
-              <li className="nav-item"><NavLink to="/invoice">Facture</NavLink>
-              </li>
-              <li className="nav-item"><NavLink to="/customer">Clients</NavLink>
-              </li>
+              <li className="nav-item"><NavLink to="/order">Commande</NavLink></li>
+              <li className="nav-item"><NavLink to="/invoice">Facture</NavLink></li>
+              <li className="nav-item"><NavLink to="/customer">Clients</NavLink></li>
             </ul>
             <div className="dropdown">
-              <a href="#"
-                 className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-                 id="dropdownUser1" data-bs-toggle="dropdown"
-                 aria-expanded="false">
+              <a href="#" className="d-flex align-items-center text-white text-decoration-none">
                 <img src="https://github.com/mdo.png" alt="" width="32"
                      height="32" className="rounded-circle me-2"/>
                 <strong>mdo</strong>
               </a>
-              <ul className="dropdown-menu dropdown-menu-dark text-small shadow"
-                  aria-labelledby="dropdownUser1">
-                <li><a className="dropdown-item" href="#">New project...</a>
-                </li>
-                <li><a className="dropdown-item" href="#">Settings</a></li>
-                <li><a className="dropdown-item" href="#">Profile</a></li>
-                <li>
-                  <hr className="dropdown-divider"/>
-                </li>
-                <li><a className="dropdown-item" href="#">Sign out</a></li>
-              </ul>
             </div>
           </div>
         </>
