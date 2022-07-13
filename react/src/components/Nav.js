c2efedeebf5e2d0299f187f936aa2090c67fd852
Nicolas Maligue <nico.maligue@gmail.com>
2022-07-13 15:25:27 +0200

    Changement de structure de la barre de Navigation

--- react/src/components/Nav.js
+++ react/src/components/Nav.js
@@ -1,9 +1,5 @@
 import React, {Component} from 'react';
-import {NavLink, Route, Routes} from 'react-router-dom';
-import Customer from './pages/Customer';
-import Quote from './pages/Quote';
-import Order from './pages/Order';
-import Invoice from './pages/Invoice';
+import { NavLink } from 'react-router-dom';
 
 class Nav extends Component {
   constructor(props) {
@@ -14,50 +10,27 @@ class Nav extends Component {
   render() {
     return (
         <>
-          <nav className="navbar navbar-dark bg-dark">
-            <a className="navbar-brand ps-3" href="index.html">Start
-              Bootstrap</a>
-          </nav>
-          <div
-              className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark w-25">
-            <a href="/"
-               className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-              <svg className="bi me-2" width="40" height="32">
-                <use href="#bootstrap"></use>
-              </svg>
-            </a>
-            <ul className="nav nav-pills flex-column mb-auto">
-              <li className="nav-item"><NavLink to="/">Dashboard</NavLink></li>
-              <li className="nav-item"><NavLink to="/quote">Devis</NavLink></li>
-              <li className="nav-item"><NavLink to="/order">Commande</NavLink>
-              </li>
-              <li className="nav-item"><NavLink to="/invoice">Facture</NavLink>
-              </li>
-              <li className="nav-item"><NavLink to="/customer">Clients</NavLink>
-              </li>
-            </ul>
-            <div className="dropdown">
-              <a href="#"
-                 className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-                 id="dropdownUser1" data-bs-toggle="dropdown"
-                 aria-expanded="false">
-                <img src="https://github.com/mdo.png" alt="" width="32"
-                     height="32" className="rounded-circle me-2"/>
-                <strong>mdo</strong>
-              </a>
-              <ul className="dropdown-menu dropdown-menu-dark text-small shadow"
-                  aria-labelledby="dropdownUser1">
-                <li><a className="dropdown-item" href="#">New project...</a>
-                </li>
-                <li><a className="dropdown-item" href="#">Settings</a></li>
-                <li><a className="dropdown-item" href="#">Profile</a></li>
-                <li>
-                  <hr className="dropdown-divider"/>
-                </li>
-                <li><a className="dropdown-item" href="#">Sign out</a></li>
-              </ul>
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Fifth navbar example">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Expand at lg</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+
+              <div class="collapse navbar-collapse" id="navbarsExample05">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item"><NavLink to="/">Dashboard</NavLink></li>      
+                  <li className="nav-item"><NavLink to="/quote">Devis</NavLink></li>
+                  <li className="nav-item"><NavLink to="/order">Commande</NavLink></li>
+                  <li className="nav-item"><NavLink to="/invoice">Facture</NavLink></li>
+                  <li className="nav-item"><NavLink to="/customer">Clients</NavLink></li>
+                </ul>
+                <form>
+                  <input class="form-control" type="text" placeholder="Search" aria-label="Search" />
+                </form>
+              </div>
             </div>
-          </div>
+          </nav>
         </>
     );
   }
