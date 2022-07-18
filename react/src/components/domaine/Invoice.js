027a79843bae62dcf080831cb8140d8e21475905
Anthony Desmartin <desmartin.anthony@gmail.com>
2022-07-18 15:33:20 +0200

    invoice update

--- react/src/components/domaine/Invoice.js
+++ react/src/components/domaine/Invoice.js
@@ -7,6 +7,7 @@ const Invoice = (props) => {
             <td>{props.invoice.id}</td>
             <td>{props.invoice.order.quote.customer.lastname}</td>
             <td>{props.invoice.order.quote.customer.firstname}</td>
+            <td>{props.invoice.order.quote.vehicle.model} {props.invoice.order.quote.vehicle.manufacturer}</td>
             <td>
                 {date.toLocaleDateString()}
             </td>
